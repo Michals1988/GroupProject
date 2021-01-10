507e69644a5a74b751e4180f29b823242c15f273
Patryk Pyka <patrykpyka1991@gmail.com>
2021-01-10 11:34:28 +0100

    change DB password

--- przepisy_web/src/com/przepisy/connection/ConnectionProvider.java
+++ przepisy_web/src/com/przepisy/connection/ConnectionProvider.java
@@ -4,7 +4,7 @@ public interface ConnectionProvider {
 	
 	String DRIVER="com.mysql.cj.jdbc.Driver";  
 	String CONNECTION_URL="jdbc:mysql://localhost:3306/przepisy?allowPublicKeyRetrieval=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false";  
-	String USERNAME="root";  
+	String USERNAME="qwerty";  
 	String PASSWORD="root";  
   
 }  
